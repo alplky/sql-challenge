71b6ab9fbfb85306d582e6b5a492ee9d86925c18
alplky <alicia.pelkey@gmail.com>
2020-10-12 16:00:46 -0500

    all employees in the sales and development departments #7

--- EmployeeSQL/employees_queries.sql
+++ EmployeeSQL/employees_queries.sql
@@ -42,3 +42,13 @@ ON e.emp_no = de.emp_no
 JOIN departments d
 ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales';
+
+-- employees in Sales and Development departments (emp no, last name, first name, dept name)
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de 
+ON e.emp_no = de.emp_no
+JOIN departments d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
+ORDER BY d.dept_name;
