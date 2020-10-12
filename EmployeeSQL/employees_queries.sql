e8b457a8738adad34a2665669f85d8d1f6d95ddc
alplky <alicia.pelkey@gmail.com>
2020-10-12 15:18:34 -0500

    employees hired in 1986

--- EmployeeSQL/employees_queries.sql
+++ EmployeeSQL/employees_queries.sql
@@ -3,3 +3,8 @@ SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries s
 ON e.emp_no = s.emp_no;
+
+-- list first name, last name, and hire date for employees hired in 1986
+SELECT first_name, last_name, hire_date 
+FROM employees
+WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31' ;
