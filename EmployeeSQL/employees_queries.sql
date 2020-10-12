c068e591f2f92079b815a4f4de42679c12037b29
alplky <alicia.pelkey@gmail.com>
2020-10-12 15:57:25 -0500

    all employees in the sales deptartent #6

--- EmployeeSQL/employees_queries.sql
+++ EmployeeSQL/employees_queries.sql
@@ -1,16 +1,16 @@
--- list employee number, last name, first name, sex, and salary
+-- employee number, last name, first name, sex, and salary
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM employees e
 JOIN salaries s
 ON e.emp_no = s.emp_no;
 
--- list first name, last name, and hire date for employees hired in 1986
+-- first name, last name, and hire date for employees hired in 1986
 SELECT first_name, last_name, hire_date 
 FROM employees
 WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31'
 ORDER BY hire_date;
 
--- list managers in each department (dept number, dept name, emp number, last name, first name)
+-- managers in each department (dept number, dept name, emp number, last name, first name)
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
 FROM dept_manager dm
 JOIN employees e
@@ -19,7 +19,7 @@ JOIN departments d
 ON dm.dept_no = d.dept_no
 ORDER BY d.dept_name;
 
--- list department of each employee (emp number, last name, first name, dept name)
+-- department of each employee (emp number, last name, first name, dept name)
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
 FROM employees e
 JOIN dept_emp de 
@@ -28,8 +28,17 @@ JOIN departments d
 ON d.dept_no = de.dept_no
 ORDER BY d.dept_name;
 
--- list first name, last name, and sex for employees named "Hercules B."
+-- first name, last name, and sex for employees named "Hercules B."
 SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 ORDER BY last_name;
+
+-- employees in Sales department (emp no, last name, first name, dept name)
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+JOIN dept_emp de 
+ON e.emp_no = de.emp_no
+JOIN departments d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales';
