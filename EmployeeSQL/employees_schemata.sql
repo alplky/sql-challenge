a5af8ca2401f3415f76e96d696bb11989f5e554f
alplky <alicia.pelkey@gmail.com>
2020-10-12 12:42:35 -0500

    created tables in database

---
+++ EmployeeSQL/employees_schemata.sql
@@ -0,0 +1,40 @@
+-- create titles table
+CREATE TABLE titles (
+	title_id varchar(5) PRIMARY KEY,
+	title varchar(30)
+);
+
+-- create employees table
+CREATE TABLE employees (
+	emp_no varchar(10) PRIMARY KEY,
+	emp_title_id varchar(5) references titles(title_id),
+	birth_date date,
+	first_name varchar(45),
+	last_name varchar(45),
+	sex varchar(1),
+	hire_date date
+);
+
+-- create departments table
+CREATE TABLE departments (
+	dept_no varchar(5) PRIMARY KEY,
+	dept_name varchar(30)
+);
+
+-- create department managers table
+CREATE TABLE dept_manager (
+	dept_no varchar(5) references departments(dept_no),
+	dept_name varchar(30)
+);
+
+-- create department employees table
+CREATE TABLE dept_emp (
+	emp_no varchar(10) references employees(emp_no),
+	dept_no varchar(5) references departments(dept_no)
+);
+
+-- create salaries table
+CREATE TABLE salaries (
+	emp_no varchar(10) references employees(emp_no),
+	salary int
+);
