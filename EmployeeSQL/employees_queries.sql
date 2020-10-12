a231b8933c9221d4d940f9b6aa3b3cba16f039ce
alplky <alicia.pelkey@gmail.com>
2020-10-12 16:15:58 -0500

    refactored order by statements in queries

--- EmployeeSQL/employees_queries.sql
+++ EmployeeSQL/employees_queries.sql
@@ -8,7 +8,7 @@ ON e.emp_no = s.emp_no;
 SELECT first_name, last_name, hire_date 
 FROM employees
 WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31'
-ORDER BY hire_date;
+ORDER BY hire_date ASC;
 
 -- managers in each department (dept number, dept name, emp number, last name, first name)
 SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
@@ -17,7 +17,7 @@ JOIN employees e
 ON dm.emp_no = e.emp_no
 JOIN departments d
 ON dm.dept_no = d.dept_no
-ORDER BY d.dept_name;
+ORDER BY d.dept_name ASC;
 
 -- department of each employee (emp number, last name, first name, dept name)
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -26,13 +26,13 @@ JOIN dept_emp de
 ON e.emp_no = de.emp_no
 JOIN departments d
 ON d.dept_no = de.dept_no
-ORDER BY d.dept_name;
+ORDER BY d.dept_name ASC;
 
 -- first name, last name, and sex for employees named "Hercules B."
 SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
-ORDER BY last_name;
+ORDER BY last_name ASC;
 
 -- employees in Sales department (emp no, last name, first name, dept name)
 SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
@@ -51,7 +51,7 @@ ON e.emp_no = de.emp_no
 JOIN departments d
 ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development'
-ORDER BY d.dept_name;
+ORDER BY d.dept_name ASC;
 
 -- frequency count of employee last names
 SELECT last_name, count(emp_no) as num_employees_with_same_last_name
