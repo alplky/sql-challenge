b2a388d17551f4974df402da19de65b41829ffc6
alplky <alicia.pelkey@gmail.com>
2020-10-12 15:30:55 -0500

    managers in each department #3

--- EmployeeSQL/employees_queries.sql
+++ EmployeeSQL/employees_queries.sql
@@ -8,3 +8,11 @@ ON e.emp_no = s.emp_no;
 SELECT first_name, last_name, hire_date 
 FROM employees
 WHERE hire_date BETWEEN '1986-1-1' and '1986-12-31' ;
+
+-- list managers in each department (dept number, dept name, emp number, last name, first name)
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name 
+FROM dept_manager dm
+JOIN employees e
+ON dm.emp_no = e.emp_no
+JOIN departments d
+ON dm.dept_no = d.dept_no;
